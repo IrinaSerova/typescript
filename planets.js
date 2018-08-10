03eb4d84560bb97deb505ffbc160773f131700ce
Peter Horton <phorton@softstackfactory.org>
2018-08-10 05:11:24 +0000

    added new planets

--- planets.js
+++ planets.js
@@ -5,6 +5,10 @@ var Planet = /** @class */ (function () {
     }
     return Planet;
 }());
-var sun = new Planet(864, 'yellow');
-// console.log(`The planet ${sun} has width ${Sun.width} and ${Sun.color}`);
-console.log(sun.color);
+var sun = new Planet(864337, 'yellow');
+var mercury = new Planet(3032, 'dark gray');
+var venus = new Planet(7520, 'yellowish white');
+console.log("The planet Sun has width " + sun.width + " and color " + sun.color);
+console.log("The planet Mercury has width " + mercury.width + " and color " + mercury.color);
+console.log("The planet Sun has width " + venus.width + " and color " + venus.color);
+// console.log(sun.color);
--- planets.ts
+++ planets.ts
@@ -4,6 +4,11 @@ class Planet {
         this.color = color;
     }
 }
-let sun = new Planet(864, 'yellow');
-console.log(`The planet ${sun} has width ${sun.width} and ${sun.color}`);
+let sun = new Planet(864337, 'yellow');
+let mercury = new Planet(3032, 'dark gray');
+let venus = new Planet(7520, 'yellowish white');
+
+console.log(`The planet Sun has width ${sun.width} and color ${sun.color}`);
+console.log(`The planet Mercury has width ${mercury.width} and color ${mercury.color}`);
+console.log(`The planet Sun has width ${venus.width} and color ${venus.color}`);
 // console.log(sun.color);
