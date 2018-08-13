3d098a9ff09b61cabf525fcd5fa5ffd39dad84d4
Peter Horton <phorton@softstackfactory.org>
2018-08-13 07:26:00 +0000

    added method info

--- planets.ts
+++ planets.ts
@@ -1,14 +1,21 @@
 class Planet {
-    constructor(width, color){
+    constructor(width, color: string){
         this.width = width;
         this.color = color;
     }
+    public info() {
+        return `Science fact. This planet has width ${this.width} and color ${this.color}`;
+    }
 }
-let sun = new Planet(864337, 'yellow');
+
 let mercury = new Planet(3032, 'dark gray');
 let venus = new Planet(7520, 'yellowish white');
+let earth = new Planet(7917, 'blue');
 
-console.log(`The planet Sun has width ${sun.width} and color ${sun.color}`);
+
 console.log(`The planet Mercury has width ${mercury.width} and color ${mercury.color}`);
-console.log(`The planet Sun has width ${venus.width} and color ${venus.color}`);
-// console.log(sun.color);
+console.log(`The planet Venus has width ${venus.width} and color ${venus.color}`);
+console.log(`The planet Earth has width ${earth.width} and color ${earth.color}`);
+console.log(mercury.info());
+
+
